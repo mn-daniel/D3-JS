f6f7ed7370e8128ff4c531792e927d11190053c5
Daniel N <dev.mndaniel@gmail.com>
2022-12-29 12:07:19 +0300

    Circles, Ellipses

--- src/js/shapes.js
+++ src/js/shapes.js
@@ -11,4 +11,21 @@ svg.selectAll("rect")
         .attr("x", (d, i) => 60 * i)
         .attr("y", (d, i) => 300 - (d * 15));
 
+let newX = 300;
+svg.selectAll("circle.first")
+    .data(dataArray)
+    .enter().append("circle")
+        .attr("class", "first")
+        .attr("cx", (d, i) => { newX += (d*3) + (i*20); return newX; })
+        .attr("cy","100")
+        .attr("r", (d) => d * 3)
+
+let ellipseX = 600;
+svg.selectAll("ellipse")
+    .data(dataArray)
+    .enter().append("ellipse")
+        .attr("cx", (d, i) => { ellipseX += (d*3) + (i*20); return ellipseX; })
+        .attr("cy","100")
+        .attr("rx", (d) => d * 3)
+        .attr("ry", "30");
         
