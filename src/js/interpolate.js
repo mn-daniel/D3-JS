0409f0b6ad852ec45d264862cfd4866ad05fdad3
Daniel N <dev.mndaniel@gmail.com>
2022-12-29 13:51:12 +0300

    D3 Generators

---
+++ src/js/interpolate.js
@@ -0,0 +1,16 @@
+let dataArray = [{x:5,y:5},{x:10,y:15},{x:20,y:7},{x:30,y:18},{x:40,y:10}];
+
+let deviceWidthLessPadding = `${window.screen.width - 20}px`;
+
+let svg = d3.select("body").append("svg").attr("height","100%").attr("width", deviceWidthLessPadding);
+
+/* d3 line is a generator */
+let line = d3.line()
+                .x((d,i) => d.x * 6)
+                .y((d,i) => d.y * 4)
+                .curve(d3.curveCardinal);
+
+svg.append("path")
+    .attr("d", line(dataArray))
+    .attr("fill","none")
+    .attr("stroke","blue");
