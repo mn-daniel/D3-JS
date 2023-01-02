83b6bb73ca6cea116f145f83e4812e95f5557f71
Daniel N <dev.mndaniel@gmail.com>
2023-01-02 13:46:07 +0300

    Scales and Axes

--- src/js/area.js
+++ src/js/area.js
@@ -5,17 +5,48 @@ let dataYears = [
     '2000','2001','2002','2003','2004','2005','2006','2007','2008',
     '2009','2010','2011','2012','2013','2014','2015','2016'];
 
+let parseDate = d3.timeParse("%Y");
+
 let height = 200,
-    width = 200;
+    width = 500;
+
+let margin = {
+    left:50,
+    right:50,
+    top:40,
+    bottom:0
+}
+
+let y = d3.scaleLinear()
+    .domain([0,d3.max(dataArray)])
+    .range([height,0]);
+
+let x = d3.scaleTime()
+    .domain(d3.extent(dataYears, (d) => parseDate(d)))
+    .range([0,width]);   
+
+let yAxis = d3.axisLeft(y).ticks(3).tickPadding(10).tickSize(10);
+let xAxis = d3.axisBottom(x);
 
 let areaGenerator = d3.area()
-    .x((d,i) => i * 20)
+    .x((d,i) => x(parseDate(dataYears[i])))
     .y0(height)
-    .y1((d) => height - d);
+    .y1((d) => y(d));
 
 let svg = d3.select("body")
     .append("svg")
     .attr("height", "100%")
     .attr("width", deviceWidthLessPadding);
 
-svg.append("path").attr("d",areaGenerator(dataArray));
+let chartGroup = svg.append("g")
+    .attr("transform",`translate(${margin.left},${margin.top})`);
+
+chartGroup.append("path").attr("d",areaGenerator(dataArray));
+chartGroup.append("g")
+    .attr("class", "axis y")
+    .call(yAxis);
+
+chartGroup.append("g")
+    .attr("class", "axis x")
+    .call(xAxis)
+    .attr("transform",`translate(0, ${height})`);
--- src/js/shapes.js
+++ src/js/shapes.js
@@ -1,23 +1,43 @@
 let dataArray = [5, 11, 18];
+let dataDays = ["Mon", "Wed","Fri"];
+
+let rainbow = d3.scaleSequential(d3.interpolateRainbow).domain([0,10]);
+let rainbow2 = d3.scaleSequential(d3.interpolateRainbow).domain([0,3]);
+
+let x = d3.scaleBand()
+    .domain(dataDays)
+    .range([0,170])
+    .paddingInner(0.1176);
+
+let xAxis = d3.axisBottom(x);
 
 let deviceWidthLessPadding = `${window.screen.width - 20}px`;
 
 let svg = d3.select("body").append("svg").attr("height","100%").attr("width", deviceWidthLessPadding);
+
+let cat20 = d3.schemeCategory20;
+console.log(cat20);
 
 svg.selectAll("rect")
     .data(dataArray)
     .enter().append("rect")
         .attr("height", (d, i) => d * 15)
         .attr("width", "50")
-        .attr("fill", "magenta")
+        .attr("fill", (d,i) => rainbow(i))
         .attr("x", (d, i) => 60 * i)
         .attr("y", (d, i) => 300 - (d * 15));
+
+svg.append("g")
+    .attr("class","x axis hidden")
+    .attr("transform","translate(0,300)")
+    .call(xAxis);
 
 let newX = 300;
 svg.selectAll("circle.first")
     .data(dataArray)
     .enter().append("circle")
         .attr("class", "first")
+        .attr("fill", (d,i) => rainbow2(i))
         .attr("cx", (d, i) => { newX += (d*3) + (i*20); return newX; })
         .attr("cy","100")
         .attr("r", (d) => d * 3)
@@ -26,6 +46,7 @@ let ellipseX = 600;
 svg.selectAll("ellipse")
     .data(dataArray)
     .enter().append("ellipse")
+        .attr("fill",(d,i) => cat20[i])
         .attr("cx", (d, i) => { ellipseX += (d*3) + (i*20); return ellipseX; })
         .attr("cy","100")
         .attr("rx", (d) => d * 3)
